5810d2238710b2c58961bb4648a5fe959d0a2163
Zulqar7866 <2407093@cognizant.com>
2025-06-17 14:45:13 +0530

    reset

--- backend/src/main/java/com/examportal/repository/ExamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.examportal.repository;
-
-import com.examportal.model.Exam;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamRepository extends JpaRepository<Exam, Long> {
-}
